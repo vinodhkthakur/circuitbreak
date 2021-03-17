51dfdadc04e92064efe42bad21b23374bb5b34e2
vinod <vinodhthakur@gmail.com>
2021-03-16 22:53:03 -0500

    intial code

---
+++ src/test/java/com/vintech/dev/VtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vintech.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
